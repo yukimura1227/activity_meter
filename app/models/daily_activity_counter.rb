29a3a600ed08e260c35b1cae0c154f53486f8d92
yukimura1227 <takamura1227@gmail.com>
2019-10-31 23:32:17 +0900

    feat: auto set year and year_month function

--- app/models/daily_activity_counter.rb
+++ app/models/daily_activity_counter.rb
@@ -1,3 +1,11 @@
 class DailyActivityCounter < ApplicationRecord
   belongs_to :activist
+  before_save :set_year_and_year_month
+
+  private
+
+  def set_year_and_year_month
+    self.year = date.year
+    self.year_month = date.strftime('%Y-%m')
+  end
 end
